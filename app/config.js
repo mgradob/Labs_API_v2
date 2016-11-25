e2de47b778db19e23860902f1653cac2d076983c
Miguel Grado <mgradob@gmail.com>
2016-11-24 21:07:02 -0700

    Fix db url

--- app/config.js
+++ app/config.js
@@ -3,6 +3,5 @@
  */
 module.exports = {
     dbSecret: 'dsp-server',
-    dbUrl: 'mongodb://localhost:27017/labs-cuu'
-    //dbUrl: 'mongodb://test:test@ds157487.mlab.com:57487/labs-cuu'
+    dbUrl: 'mongodb://test:test@ds157487.mlab.com:57487/labs-cuu'
 };
